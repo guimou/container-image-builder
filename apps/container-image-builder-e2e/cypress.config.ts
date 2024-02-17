d9b4ea9465cc46a31f551156578014ca98ad9ec8
Guillaume Moutier <guillaume.moutier@gmail.com>
2024-02-17 11:09:29 -0500

    Initial commit

---
+++ apps/container-image-builder-e2e/cypress.config.ts
@@ -0,0 +1,18 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      bundler: 'vite',
+      webServerCommands: {
+        default: 'nx run container-image-builder:serve',
+        production: 'nx run container-image-builder:preview',
+      },
+      ciWebServerCommand: 'nx run container-image-builder:serve-static',
+    }),
+    baseUrl: 'http://localhost:4200',
+  },
+});
